22c9611429946c44d7cc23580e362b075a1655a1
Grant Draper <grant_draper@hotmail.co.uk>
2017-02-17 17:50:19 +0000

    see notes / comments

--- Z Tidying Client Server Code/Client.py
+++ Z Tidying Client Server Code/Client.py
@@ -13,13 +13,13 @@ from ClientClass import *
 
 c1 = Client("192.168.1.201", 30000)
 
-c1.listening()
+#c1.listening()
 
+print("\n")
+print("Welcome to ChatterBox", "\n", "\n")
+print("Log in page")
 
-
-
-
-
+Client.get_username()
 
 
 
--- Z Tidying Client Server Code/ClientClass.py
+++ Z Tidying Client Server Code/ClientClass.py
@@ -1,4 +1,3 @@
-
 import socket
 import select
 import ssl
@@ -13,7 +12,6 @@ import struct
 
 
 class Client:
-
     server_details = []
 
     def __init__(self, HOST, PORT):
@@ -29,6 +27,50 @@ class Client:
             print("Completed with Exception1.", "\n")
             pass
 
+    def user_log_in(self):
+
+        """need to run the get uname and pass functions, then pass it through cred
+            checker, this will validate login, then can just print logged in.
+
+            something like, if Client.get_username()[0] == True and Client.get_pass()[0] == True
+                                Client.cred_checker
+
+            see tictac toe"""
+
+
+
+
+    def get_username(self):
+
+        while True:
+            print("Please enter a Username:")
+            user = input()
+            if len(user) == 0:
+                print("Username invalid")
+                pass
+
+            elif len(user) <= 20:
+                print("ok")
+                return True, str(user)
+
+    def get_pass():
+
+        while True:
+            print("Please enter a Password:")
+            pswd = input()
+            pswd = str(pswd)
+            if len(pswd) <= 7:
+                print("Password invalid")
+                pass
+
+            elif len(pswd) > 7 and len(pswd) <= 50:
+                print("ok")
+                return True, pswd
+
+    def check_credentials(self, user, pswd):
+
+        Message.send_msg(self, Message.TYPES["USER"], user, self.ssl_socket)
+        Message.send_msg(self, Message.TYPES["PASS"], pswd, self.ssl_socket)
 
     def listening(self):
 
@@ -51,7 +93,6 @@ class Client:
             except Exception as e:
                 Client.__init__(self, Client.server_details[-2], Client.server_details[-1])
 
-
     def raw_receive(self, sock, length):
         """This function receives length bytes of raw data from a socket, returning the data."""
 
@@ -70,7 +111,6 @@ class Client:
         except Exception as e:
             print(e)
 
-
     def raw_send(self, sock, length, data):
         """ This function sends raw data on a socket."""
 
@@ -83,29 +123,26 @@ class Client:
             total_sent = total_sent + sent
 
 
-
 class Message:
-
-    TYPES = {"NORMAL": 0,          #0
-             "JOIN": 1,            #1
-             "USER": 2,            #2
-             "PASS": 3,            #3
-             "DIRECT": 4,          #4
-             "COMMAND": 5,         #5
-             "SERVER": 6}          #6
+    TYPES = {"NORMAL": 0,  # 0
+             "JOIN": 1,  # 1
+             "USER": 2,  # 2
+             "PASS": 3,  # 3
+             "DIRECT": 4,  # 4
+             "COMMAND": 5,  # 5
+             "SERVER": 6}  # 6
     HEADER_LENGTH = 8
 
     def __init__(self):
         pass
 
-
     def send_msg(self, msg_type, msg_text, sock):
         """This function sends a message to a socket."""
 
-        full_msg = struct.pack('!LL', msg_type, len(msg_text) - 1) + bytes(msg_text.strip().encode("utf-8"))  # cut off a newline
+        full_msg = struct.pack('!LL', msg_type, len(msg_text) - 1) + bytes(
+            msg_text.strip().encode("utf-8"))  # cut off a newline
 
         Client.raw_send(self, sock, len(full_msg), full_msg)
-
 
     def receive_msg_from(self, sock):
         """This function waits for a message on a socket and returns the message type and text."""
@@ -120,7 +157,6 @@ class Message:
         except MemoryError as err:
             print("MemoryError: " + err.message)
             return None
-
 
     def print_message(self, msg_type, msg_text):
         """This function prints a message with the text length in a nice format."""
