aea9ff3f3ecb6fddb966eb84a41ad3da41fd0b48
Grant Draper <grant_draper@hotmail.co.uk>
2017-02-17 15:44:23 +0000

    server and client both completed with single class imports
    exception handling added to the server to stop crashes after socket connection broken by removing the socket from the list,
    
     handling also added for client to loop until the server comes back online. very rudimentary (needs revision if have time)

--- Z Tidying Client Server Code/ClientClass.py
+++ Z Tidying Client Server Code/ClientClass.py
@@ -5,19 +5,24 @@ import ssl
 import sys
 import struct
 
+
 # Message format is:
 #   4 bytes unsigned type
 #   4 bytes unsigned length
 #   Message data
 
+
 class Client:
 
+    server_details = []
 
     def __init__(self, HOST, PORT):
+
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.ssl_socket = ssl.wrap_socket(server_socket, ca_certs="server.crt", cert_reqs=ssl.CERT_REQUIRED)
             self.ssl_socket.connect((HOST, PORT))
+            Client.server_details += HOST, PORT
 
         except Exception as e:
             print(e)
@@ -31,18 +36,20 @@ class Client:
 
             # 6. check if input has been received from stdin or the server_socket
             available_streams, _, _ = select.select([sys.stdin, self.ssl_socket], [], [], 1)
+            try:
+                # 7. if sys.stdin is available to read, read from it and send the message
+                if sys.stdin in available_streams:
+                    msg_text = sys.stdin.readline()
+                    Message.send_msg(self, Message.TYPES["NORMAL"], msg_text, self.ssl_socket)
+                    continue
 
-            # 7. if sys.stdin is available to read, read from it and send the message
-            if sys.stdin in available_streams:
-                msg_text = sys.stdin.readline()
-                Message.send_msg(self, Message.TYPES["NORMAL"], msg_text, self.ssl_socket)
-                continue
+                # 8. if the server socket is available to read, read from it and print the message
+                if self.ssl_socket in available_streams:
+                    msg_type, msg_text = Message.receive_msg_from(self, self.ssl_socket)
+                    Message.print_message(self, msg_type, msg_text)
 
-            # 8. if the server socket is available to read, read from it and print the message
-            if self.ssl_socket in available_streams:
-                msg_type, msg_text = Message.receive_msg_from(self, self.ssl_socket)
-                Message.print_message(self, msg_type, msg_text)
-
+            except Exception as e:
+                Client.__init__(self, Client.server_details[-2], Client.server_details[-1])
 
 
     def raw_receive(self, sock, length):
@@ -51,13 +58,17 @@ class Client:
         chunks = []
         bytes_rx = 0
 
-        while bytes_rx < length:
-            chunk = sock.recv(length - bytes_rx)
-            if chunk == b'':
-                raise RuntimeError("Socket connection broken")
-            chunks.append(chunk)
-            bytes_rx += len(chunk)
-        return b''.join(chunks)
+        try:
+            while bytes_rx < length:
+                chunk = sock.recv(length - bytes_rx)
+                if chunk == b'':
+                    raise RuntimeError("Socket connection broken")
+                chunks.append(chunk)
+                bytes_rx += len(chunk)
+            return b''.join(chunks)
+
+        except Exception as e:
+            print(e)
 
 
     def raw_send(self, sock, length, data):
@@ -70,9 +81,6 @@ class Client:
             if sent == 0:
                 raise RuntimeError("Socket send failure")
             total_sent = total_sent + sent
-
-
-
 
 
 
@@ -94,7 +102,6 @@ class Message:
     def send_msg(self, msg_type, msg_text, sock):
         """This function sends a message to a socket."""
 
-        #print("sending ", msg_text)
         full_msg = struct.pack('!LL', msg_type, len(msg_text) - 1) + bytes(msg_text.strip().encode("utf-8"))  # cut off a newline
 
         Client.raw_send(self, sock, len(full_msg), full_msg)
@@ -119,6 +126,5 @@ class Message:
         """This function prints a message with the text length in a nice format."""
 
         print((next(iter({k for k, v in Message.TYPES.items() if v == msg_type}))), len(msg_text), msg_text)
-
 
 
